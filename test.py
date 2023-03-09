113e71c6b09f3a8cbe894cc5e923128c00d5ee18
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2023-03-09 18:42:29 +0100

    cleanup test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2022 The Ikarus Developers mueller@ibb.uni-stuttgart.de
 # SPDX-License-Identifier: LGPL-3.0-or-later
-
+import matplotlib
 import dune.grid
 import dune.functions
 import pyikarus as iks
@@ -10,17 +10,17 @@ import pyikarus.assembler
 import pyikarus.dirichletValues
 import numpy as np
 import scipy as sp
-
+import time
 
 from dune.vtk import vtkUnstructuredGridWriter, vtkWriter, RangeTypes, FieldInfo
 
 if __name__ == "__main__":
-    print("test")
     # help(iks)
+    tic = time.perf_counter()
     lowerLeft = []
     upperRight = []
 
-    
+
     elements = []
     for i in range(2):
         lowerLeft.append(-1)
@@ -31,26 +31,18 @@ if __name__ == "__main__":
     req.addAffordance(iks.ScalarAffordances.mechanicalPotentialEnergy)
 
     grid = dune.grid.structuredGrid(lowerLeft,upperRight,elements)
-    grid. hierarchicalGrid . globalRefine(6)
-    basisLagrange1 = dune.functions.defaultGlobalBasis(grid, dune.functions.Power(dune.functions.Lagrange(order=1),2))
+    grid.hierarchicalGrid.globalRefine(6)
 
+    basisLagrange1 = dune.functions.defaultGlobalBasis(grid, dune.functions.Power(dune.functions.Lagrange(order=1),2))
+    print('We have {} dofs.'.format(len(basisLagrange1)))
+    print('We have {} vertices.'.format(grid.size(2)))
+    print('We have {} elements.'.format(grid.size(0)))
     d = np.zeros(len(basisLagrange1))
-    d[0]=0.0
 
     lambdaLoad = iks.ValueWrapper(3.0)
     req.insertParameter(iks.FEParameter.loadfactor,lambdaLoad)
 
-    assert req.getParameter(iks.FEParameter.loadfactor) == lambdaLoad
     req.insertGlobalSolution(iks.FESolutions.displacement,d)
-
-    d2= req.getGlobalSolution(iks.FESolutions.displacement)
-
-    assert ('{}'.format(hex(d2.__array_interface__['data'][0]))) == ('{}'.format(hex(d.__array_interface__['data'][0])))
-    assert len(d2)== len(d)
-    assert (d2== d).all()
-    fes = []
-    forces = np.zeros(8)
-    stiffness = np.zeros((8,8))
 
     def volumeLoad(x,lambdaVal) :
         return np.array([lambdaVal*x[0]*2, 2*lambdaVal*x[1]*0])
@@ -58,43 +50,30 @@ if __name__ == "__main__":
     def neumannLoad(x,lambdaVal) :
         return np.array([lambdaVal*0, lambdaVal])
 
-
     neumannVertices = np.zeros(grid.size(2)*2, dtype=bool)
-    assert len(neumannVertices)== len(basisLagrange1)
 
     basisLagrange1.interpolate(neumannVertices, lambda x :  True  if x[1]>0.9 else False)
 
     boundaryPatch = iks.utils.boundaryPatch(grid,neumannVertices)
 
-    # the following should throw
-    try:
-        iks.finite_elements.linearElasticElement(basisLagrange1,grid.elements[0],1000,0.2,volumeLoad,boundaryPatch)
-        assert False
-    except TypeError :
-        assert True
-
+    fes = []
     for e in grid.elements:
         fes.append(iks.finite_elements.linearElasticElement(basisLagrange1,e,1000,0.2,volumeLoad,boundaryPatch,neumannLoad))
 
+    forces = np.zeros(8)
+    stiffness = np.zeros((8,8))
     fes[0].calculateVector(req,forces)
     fes[0].calculateMatrix(req,stiffness)
-    fes[0].localView()
+    np.set_printoptions(precision=3)
+    print('Forces:\n {}'.format(forces))
+    print('Stiffness:\n {}'.format(stiffness))
 
-    dirichletValues = iks.dirichletValues.dirichletValues(basisLagrange1) # TODO make second redundant
-
-    def fixFirstIndex(vec,globalIndex):
-        vec[0]= True
-
-    def fixAnotherVertex(vec,localIndex,localView):
-        localView.index(localIndex)
-        vec[1]= True
+    dirichletValues = iks.dirichletValues.dirichletValues(basisLagrange1) 
 
     def fixLeftHandEdge(vec,localIndex,localView,intersection):
         if (intersection.geometry.center[1]<-0.9):
             vec[localView.index(localIndex)]= True
 
-    dirichletValues.fixBoundaryDOFs(fixFirstIndex)
-    dirichletValues.fixBoundaryDOFsUsingLocalView(fixAnotherVertex)
     dirichletValues.fixBoundaryDOFsUsingLocalViewAndIntersection(fixLeftHandEdge)
 
     assembler = iks.assembler.sparseFlatAssembler(fes,dirichletValues)
@@ -102,17 +81,11 @@ if __name__ == "__main__":
 
     Msparse = assembler.getMatrix(req)
     forces = assembler.getVector(req)
-    # Mdense = assemblerDense.getMatrix(req)
-    # assert (Msparse == Mdense).all()
-    # print(Mdense)
-    # print("F:",forces)
+    print(Msparse)
 
     x = sp.sparse.linalg.spsolve(Msparse,-forces)
-    # print(x)
     fx = basisLagrange1.asFunction(x)
-    grid.plot()
-    # grid.writeVTK(pointdata={"fx":fx},name="nameTest",number=1)
-    writer = vtkWriter( grid, "nameTest",
-                    pointData   = {( "displacement",(0,1)):fx}
-
-                    )
+    #grid.plot()
+    writer = vtkWriter( grid, "nameTest",  pointData   = {( "displacement",(0,1)):fx})
+    toc = time.perf_counter()
+    print(f"The whole code took {toc - tic:0.4f} seconds to excecute")
